e2134775f5368e555ce010aa2498f65f6b2f4d92
raudelgar <raudel24@gmail.com>
2020-04-01 10:26:27 -0400

    styling navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
+	const navLinkStyle = {
+		fontWeight: 'bold',
+		color: '#bb2e1f',
+		textDecoration: 'none'
+	};
 	return (
 		<nav className='row space-between'>
 			<ul className='row nav'>
-				<li>Top</li>
-				<li>New</li>
+				<li>
+					<NavLink
+						to='/'
+						activeClassName='selected'
+						className='navbar-light'
+						exact
+					>
+						Top
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						to='/new'
+						activeClassName='selected'
+						className='navbar-light'
+						exact
+					>
+						New
+					</NavLink>
+				</li>
 			</ul>
 		</nav>
 	);
